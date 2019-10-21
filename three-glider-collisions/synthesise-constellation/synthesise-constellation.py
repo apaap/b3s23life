e4fb2d44db3ae1ab1e310d1843ccfb686fa77c73
Arie Paap <wildmyron@gmail.com>
2019-10-21 13:35:40 +0800

    Separate 2G collisions and add a subset of 4G collisions in sythesise-constellation

--- three-glider-collisions/synthesise-constellation/synthesise-constellation.py
+++ three-glider-collisions/synthesise-constellation/synthesise-constellation.py
@@ -3,6 +3,7 @@
 #   http://www.conwaylife.com/forums/viewtopic.php?p=68370#p68370
 # v2.0 update by Arie Paap, changelog:
 #   - Use sjk component format for glider collisions
+#   - Include a subset of 4G collisions
 
 import os
 import re
@@ -10,18 +11,29 @@ import golly as g
 
 g.setrule("B3/S23")
 
-# File containing glider collisions data.
-compFile = "3Gcols.sjk"
+# Files with glider collisions data. This file contains the 2G collisions
+compFiles = ["3Gcols.sjk", "4Gcols.sjk"]
 
 def main():
     apgcode = canonise()
     
     compFormat = re.compile('^([a-z0-9_]*)>([ 0-9\/-]+)>{}$'.format(apgcode))
     cols = []
-    with open(compFile) as cF:
-        for line in cF:
-            m = compFormat.search(line)
-            if m: cols.append(m.groups())
+    # 2G collisions
+    for line in twoGcols:
+        m = compFormat.search(line)
+        if m: cols.append(m.groups())
+    # 3G and 4G collisions
+    for cFile in compFiles:
+        with open(cFile) as cF:
+            for line in cF:
+                m = compFormat.search(line)
+                if m:
+                    mg = m.groups
+                    if mg[0]:
+                        g.warn('Non-empty starting target in glider collision - Not implemented')
+                        continue
+                    cols.append(mg)
     if cols:
         # g.note(str(cols[:20]))
         cols = [reconstruct(col[1]) for col in cols]
@@ -36,6 +48,42 @@ def main():
         g.fit()
     else:
         g.note("No glider collisions found for that constellation. Better luck next time")
+
+twoGcols = """
+>9 -1//7 -1/>xp2_7
+>9 -1/1 2//>xp2_7
+>8 0/-4 3//>xp2_7
+>8 0///6 -2>xs6_696
+>9 -2//6 -1/>xp2_s01110szw222
+>9 -3/6 0//>xp2_s01110szw222
+>6 0///4 -3>xp2_s01110szw222
+>7 0//3 0/>xs4_33
+>9 -2//6 -2/>xs4_33
+>9 -3//6 -2/>xs4_33
+>8 0/-6 3//>xs4_33
+>8 -3/4 1//>xs4_33
+>7 0///0 -2>xs4_33
+>8 -1//6 0/>xs7_2596
+>9 -1//6 -1/>xs5_253
+>9 -1/-2 3//>xs24_y1696z2552wgw2552zy1343
+>9 -2/1 2//>xs24_y1696z2552wgw2552zy1343
+>9 -1/-3 3//>xs24_y1696z2552wgw2552zy1343
+>9 0//4 0/>xs24_y1696z2552wgw2552zy1343
+>9 -1//8 -1/>xs24_y1696z2552wgw2552zy1343
+>8 0/-3 3//>xs16_0ggydgj3zop1yd11
+>9 -2//8 -2/>xs8_6996
+>9 -2/2 2//>xs8_6996
+>6 -1/0 2//>xs8_rr
+>8 0///4 -2>xs8_33w66
+>9 -2/-1 2//>xp2_ggg07y270gggzy0ey2e
+>8 -1///4 -3>xp2_7zw6952
+>8 -1///0 -2>xp2_7zw6952
+>9 -1/-4 3//>xs12_2552zy2696
+>9 -1/-5 3//>xp2_xccy3252zgw8kicz3
+>9 -3/4 1//>xs7_178c
+>9 -3//5 -2/>xs16_ooy033zy1ooy033
+>8 0//6 0/>xp2_yj2552z696ycezy0888y41110s0111zw70ggg07yh696zzybg8gzyb121
+"""
 
 r270 = ( 0, -1,  1,  0)
 r180 = (-1,  0,  0, -1)
@@ -52,7 +100,7 @@ def reconstruct(gstr, stepback=2):
     # Process transformation
     # XXX unimplemented (but not required here)
     t, o, shift_x, shift_y = 0, "identity", 0, 0
-    # Step back to separate gliders (same as Shinjuku uses for realising syntheses 
+    # Step back to separate gliders (same as Shinjuku uses for realising syntheses)
     t += stepback
     # Process glider sets
     for i, field in enumerate(gstr.split("/")):
@@ -97,7 +145,7 @@ def bijoscar(maxsteps):
 
 def canonise():
     
-    p = bijoscar(4)
+    p = bijoscar(6)
     
     representation = "#"
     for i in range(abs(p)):
